0c79cc11a6da5e1ce08374f6b136d503f70b7ff6
SHARLDEGOLL1926 <94676999+SHARLDEGOLL1926@users.noreply.github.com>
2022-05-03 18:56:51 +0900

    Add files via upload

---
+++ data/shops.py
@@ -0,0 +1,19 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from data.db_session import SqlAlchemyBase
+
+
+class Shop(SqlAlchemyBase):
+    __tablename__ = 'shops'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    coordinats = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
